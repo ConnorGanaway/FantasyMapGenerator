220ffc77352ea2203f9fe3a5b2f38ea7db299940
Connor Ganaway <connorganaway@gmail.com>
2022-04-05 20:48:49 -0700

    Removed unused hash function implementation

--- FantasyMapGenerator/FantasyMapGenerator/MainWindow.xaml.cs
+++ FantasyMapGenerator/FantasyMapGenerator/MainWindow.xaml.cs
@@ -33,7 +33,6 @@ namespace FantasyMapGenerator
         public MainWindow()
         {
             InitializeComponent();
-            //DataContext = new DataStuff("../../../new_map.png");
             this.SizeToContent = SizeToContent.Manual;
             scale.Value = 100;
             octaves.Value = 7;
@@ -101,7 +100,7 @@ namespace FantasyMapGenerator
                     stringChars[i] = chars[random.Next(chars.Length)];
                 }
 
-                tempSeed = new String(stringChars);
+                tempSeed = new String(stringChars) + "_____";
             }
 
 
@@ -109,21 +108,6 @@ namespace FantasyMapGenerator
 
             File.WriteAllTextAsync("settingsTemp.json", str);
             SeedInput.Text = tempSeed;
-
-            DateTime time = DateTime.Now;
-            tempSeed = tempSeed + time.ToString("h:mm:ss tt");
-            string filename = "nm_" + HashString(tempSeed) + ".png";
-
-            string dir = "/maps";
-
-            if (!Directory.Exists(dir)){
-                Directory.CreateDirectory(dir);
-            }else{
-                Directory.Delete(dir, true);
-                Directory.CreateDirectory(dir);
-            }
-
-            string fullPath = dir + "\\" + filename;
 
             System.Diagnostics.ProcessStartInfo procStartInfo = new System.Diagnostics.ProcessStartInfo("cmd", "/C python3 ../../../../../noise_gen.py settingsTemp.json");
             procStartInfo.CreateNoWindow = true;
@@ -254,29 +238,6 @@ namespace FantasyMapGenerator
             if (lacunarity != null)
             {
                 lacunarity.Value = Math.Round(lacunarity.Value, 2);
-            }
-        }
-
-        static string HashString(string text, string salt = "")
-        {
-            if (String.IsNullOrEmpty(text))
-            {
-                return String.Empty;
-            }
-
-            // Uses SHA256 to create the hash
-            using (var sha = new System.Security.Cryptography.SHA256Managed())
-            {
-                // Convert the string to a byte array first, to be processed
-                byte[] textBytes = System.Text.Encoding.UTF8.GetBytes(text + salt);
-                byte[] hashBytes = sha.ComputeHash(textBytes);
-
-                // Convert back to a string, removing the '-' that BitConverter adds
-                string hash = BitConverter
-                    .ToString(hashBytes)
-                    .Replace("-", String.Empty);
-
-                return hash;
             }
         }
 
@@ -452,9 +413,6 @@ namespace FantasyMapGenerator
 
         private void Create_Report_Button_Click(object sender, RoutedEventArgs e)
         {
-            // Displays a SaveFileDialog so the user can save the Image
-            // assigned to Button2.
-            Stream stream;
             SaveFileDialog saveFileDialog1 = new SaveFileDialog();
             saveFileDialog1.Filter = "PNG Image|*.png|JPeg Image|*.jpg";
             saveFileDialog1.Title = "Save an Image File";
